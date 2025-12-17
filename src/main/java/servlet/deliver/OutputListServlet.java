3783bdbe0129ef264ef1b785cf2d451067f21928
JC21-230023 <230023@jc-21.jp>
2025-12-17 16:32:51 +0900

    一覧の中身を書込み

--- src/main/java/servlet/deliver/OutputListServlet.java
+++ src/main/java/servlet/deliver/OutputListServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import sql_temp.ConnectSQL;
+
 /**
  * Servlet implementation class OutputListServlet
  */
-@WebServlet("/OutputListServlet")
+@WebServlet("/delivers")
+//担当者がアクセスの想定、業者、日付を選択する
+
 public class OutputListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public OutputListServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -27,15 +27,23 @@ public class OutputListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/Outputlist.jsp");
+		
+		try {
+			
+			ConnectSQL.getSt("");
+		}catch(Exception e) {
+			
+			
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
+		
 		doGet(request, response);
 	}
 
--- src/main/webapp/deliver/Outputlist.jsp
+++ src/main/webapp/deliver/Outputlist.jsp
@@ -8,9 +8,50 @@
 <!-- 配達依頼リスト表示.jsp -->
 </head>
 <body>
+<header>
+	配達依頼リスト
+<form action="/deliver/">
+	<div >
+		<select>
+			
+			<%for(配達業者:配達業者全件){%>
+			<option value=<%//業者ID%>>
+				<%//=配達業者 %>
+			</option>
+			<%//}%>
+		</select>
+	</div>
+	<div><!--  配達日付-->
+		<input type="date">
+	</div>
+	<button type="submit">一覧表示</button>
+</form>
 
-
-
-
+<div><%//＝配達件数 %></div>
+</header>
+<!-- 区切り -->
+<table>
+<tr>
+	<td class="back1">No</td>
+	<td class="back2">配達先情報</td>
+	<td class="back3">予約番号</td>
+	<td class="back4">家具の個数</td>
+	<td class="back5">家具番号</td>
+	
+</tr>
+<%
+for (String[] ss : productList) {
+%>
+<tr>
+	<td class="back1"><%=ss[0]%></td>
+	<td class="back2"><%=ss[1]%></td>
+	<td class="back3"><%=ss[2]%></td>
+	<td class="back4"><%=ss[3]%></td>
+	<td class="back5"><%=ss[4]%></td>
+</tr>
+<%
+}%>
+</table>
+<button><a></a> </button>
 </body>
 </html>
